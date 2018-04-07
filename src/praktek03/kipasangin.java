4409194e95b9769deccc2f85f3ee4d365dc14e87
hilmanhamidi <hilmanhamidi007@gmail.com>
2018-04-07 10:36:22 +0800

    buat getter and setter

--- src/praktek03/kipasangin.java
+++ src/praktek03/kipasangin.java
@@ -14,4 +14,37 @@ void cetakInfo(){
             "Harga \t           : " +harga);
         
 }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getBentuk() {
+        return bentuk;
+    }
+
+    public void setBentuk(String bentuk) {
+        this.bentuk = bentuk;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
 }
