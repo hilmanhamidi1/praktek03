883d3b0ad4ca16f9618ba083fc62dcec61b8d4cf
hilmanhamidi <hilmanhamidi007@gmail.com>
2018-04-07 10:59:22 +0800

    menggunakan method get

--- src/praktek03/kipasanginaksi.java
+++ src/praktek03/kipasanginaksi.java
@@ -8,6 +8,16 @@ public class kipasanginaksi {
         k.setHarga(300000);
         
         k.cetakInfo();
+        System.out.println("--------------------");
+        System.out.println("Merknya \t     :");
+        System.out.println(k.getMerk());
+        System.out.println("Warnanya \t     :");
+        System.out.println(k.getWarna());
+        System.out.println("Bentuknya \t     :");
+        System.out.println(k.getBentuk());
+        System.out.println("Harganya \t     :");
+        System.out.println(k.getHarga());
+        
     }
       
 }
